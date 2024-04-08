21c01665663c214d4a48ae4a9e8e71c27a52374c
FahimReza <FahimReza202@outlook.com>
2024-04-09 00:42:58 +0330

    Saved Ended Changes in Login and Logout

--- Website/Store/views.py
+++ Website/Store/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render , redirect
 from . import models
 from .models import Product
-
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -15,8 +16,22 @@ def About(request):
 
 
 def Login_User(request):
-    return render(request=request , template_name="Login.html" , context={})
+    if request.method == 'POST':
+        username = request.POST['UserName']
+        password = request.POST['Password']
+        user = authenticate(request , username=username , password=password)
+        if user is not None:
+            login(request , user)
+            messages.error(request , "!! تبریک ! با موفقیت به حساب خود وارد شدید ")
+            return redirect('/')
+        else:
+            messages.error(request , 'حسابتون وجود ندارد!!')
+            return redirect('/Login/')
 
+    else:
+        return render(request=request , template_name="Login.html" , context={})
 
 def Logout_User(request):
-    return render(request=request , template_name="Logout.html" , context={})
+    logout(request)
+    messages.warning(request , '!! تبریک ! با موفقیت از حساب خود خارج شدید ')
+    return redirect('/')
